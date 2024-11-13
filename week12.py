1de60dd52745bf4d90d1b1ae35b048e0f0330dbf
dayeon29 <ashley200307@naver.com>
2024-11-13 10:55:40 +0900

    v.05 prime number

--- week12.py
+++ week12.py
@@ -1,12 +1,16 @@
 counts = 0
+is_prime = True
 number = int(input("Input number : "))
 
-for i in range(1, number+1):  # 1부터 입력한 수까지 반복
-    if number % i == 0:  # 입력한 수를 1부터 입력된 수까지 나누어 떨어지는지 조건문으로 체크
-        counts = counts + 1  # 약수 발견 시 카운트 값 증가
-    print(i, end=' ')
+if number <= 1:
+    is_prime = False
+else:
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+        print(i, end=' ')
 
-if counts == 2:
+if is_prime:
     print(f"{number} is prime number~")
 else:
-    print(f"{number} is NOT prime number! (divisors : {counts})")
+    print(f"{number} is NOT prime number!")
